5551dc4eface627576a5b246cd14e73666edf56e
Ivan Kozlov <ikozlov@student.42.us.org>
2019-02-18 05:43:18 -0800

    4. Median of Two Sorted Arrays. Optimized

--- leetcode/4.js
+++ leetcode/4.js
@@ -2,32 +2,42 @@
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number}
- * Time: O(N + M)
- * Space: O(1)G
+ * Time: O(log(min(N, M)))
+ * Space: O(1)
+ * Where N and M lengths of the first and second arrays accordingly
  */
 var findMedianSortedArrays = function(nums1, nums2) {
-  [nums1, nums2] = nums1.length > nums2.length
-      ? [nums1, nums2]
-      : [nums2, nums1];
-  let i = 0, j = 0;
-  let merged = [];
-  while (i < nums1.length && j < nums2.length) {
-      if (nums1[i] > nums2[j]) {
-          merged.push(nums2[j++]);
-      } else {
-          merged.push(nums1[i++]);
+  if (nums1.length > nums2.length)
+      return findMedianSortedArrays(nums2, nums1);
+  
+  const x = nums1.length;
+  const y = nums2.length;
+  const totalLen = x + y;
+  const half = Math.floor((x + y + 1) / 2);
+  let low = 0;
+  let high = x;
+  
+  while (low <= high) {
+      let px = Math.floor((high + low) / 2);
+      let py = half - px;
+      if (px < x && nums2[py - 1] > nums1[px])
+          low = px + 1;
+      else if (px > 0 && nums1[px - 1] > nums2[py])
+          high = px - 1;
+      else {
+          let maxLeft, minRight;
+          if (px === 0) maxLeft = nums2[py - 1]
+          else if (py === 0) maxLeft = nums1[px - 1]
+          else maxLeft = Math.max(nums1[px - 1], nums2[py - 1]);
+
+          if (px === x) minRight = nums2[py]
+          else if (py === y) minRight = nums1[px]
+          else minRight = Math.min(nums1[px], nums2[py]);
+          if (totalLen % 2 === 1)
+              return maxLeft;
+          else
+              return (maxLeft + minRight) / 2
       }
   }
-  while (i < nums1.length) {
-      merged.push(nums1[i++]);
-  }
-  while (j < nums2.length) {
-      merged.push(nums2[j++]);
-  }
-  let middle = Math.floor(merged.length / 2);
-  let median = merged[middle];
-  if (merged.length % 2 === 0) {
-      median = (median + merged[middle - 1]) / 2;
-  }
-  return median;
+  return -1;
 };
