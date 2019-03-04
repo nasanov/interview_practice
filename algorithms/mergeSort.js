d2e689a4e002f29530f081f53d94447a34407834
Ivan Kozlov <ikozlov@student.42.us.org>
2019-03-03 21:28:58 -0800

    Refactor. Added cmp function to mergeSort

--- algorithms/mergeSort.js
+++ algorithms/mergeSort.js
@@ -1,38 +1,32 @@
-const _merge = (a1, a2) => {
-  const res = [];
-  let i = j = 0;
-  while (a1[i] && a2[j]) {
-    if (a1[i] >= a2[j])
-      res.push(a1[i++]);
-    else
-      res.push(a2[j++])
+const defaultCmp = (a, b) => a - b;
+
+const mergeSortRec = (array, l, r, cmp) => {
+  const _merge = (main, k, a1, a2) => {
+    let i = j = 0;
+    while (a1[i] && a2[j]) {
+      main[k++] = cmp(a1[i], a2[j]) >= 0 ? a1[i++] : a2[j++];
+    }
+    while (a1[i]) main[k++] = a1[i++];
+    while (a2[j]) main[k++] = a2[j++];
   }
-  while (a1[i]) res.push(a1[i++]);
-  while (a2[j]) res.push(a2[j++]);
-  console.log(res);
-  return res;
+
+  if (l < r) {
+    const mid = Math.floor((l + r) / 2);
+    mergeSortRec(array, l, mid, cmp);
+    mergeSortRec(array, mid + 1, r, cmp);
+    merged = _merge(array, l, array.slice(l, mid + 1), array.slice(mid + 1, r + 1));
+  }
 }
 
-const mergeSort = (array) => {
-  const mergeSortRec = (array) => {
-    const l = 0, r = array.length - 1;
-    let merged = array;
-    if (l < r) {
-      const mid = (l + r) / 2;
-      const left = mergeSortRec(array.slice(0, l + 1));
-      const right = mergeSortRec(array.slice(-r));
-      merged = _merge(left, right);
-    }
-    return merged;
-  }
-  return mergeSortRec(array, 0, array.length - 1);
+const mergeSort = (array, cmp = defaultCmp) => {
+  return mergeSortRec(array, 0, array.length - 1, cmp);
 }
 
 function main() {
-  const array = [10, 80, 30, 90, 40, 50, 70];
-  const sorted = mergeSort(array);
-  console.log(`Original ${array}\nvs\nSorted ${sorted}`);
-  return sorted;
+  const array = [38, 27, 43, 3, 9, 82, 10];
+  const cpy = [...array];
+  mergeSort(cpy);
+  console.log(`Original ${array}\nvs\nSorted ${cpy}`);
 }
 main()
 
