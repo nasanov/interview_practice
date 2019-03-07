7a47aaa6b3777173fd405471ee5fd5994806312f
Ivan Kozlov <ikozlov@student.42.us.org>
2019-03-07 03:57:15 -0800

    Added min heap

---
+++ data_structures/minheap.js
@@ -0,0 +1,45 @@
+class MinHeap {
+  constructor() {
+    this.heap = [null];
+  }
+
+  insert(val) {
+    this.heap.push(val);
+    let idx = this.heap.length - 1;
+    let parentIdx = Math.floor(idx / 2);
+    while (Number.isInteger(this.heap[parentIdx])
+      && val < this.heap[parentIdx]) {
+        this.heap[idx] = this.heap[parentIdx];
+        this.heap[parentIdx] = val;
+        idx = parentIdx;
+        parentIdx = Math.floor(idx / 2);
+      }
+  }
+
+  getMin() {
+    let res;
+    if (this.heap.length <= 2) {
+      res = this.heap.pop();
+      this.heap[0] = null;
+    } else {
+      res = this.heap[1];
+      this.heap[1] = this.heap.pop();
+      this._heapify(1);
+    }
+    return res;
+  }
+
+  _heapify(idx) {
+    const l = idx * 2;
+    const r = idx * 2;
+
+    const current = this.heap[idx];
+    const childIdx = Number.isInteger(this.heap[r]) && this.heap[r] < this.heap[l] ? r : l;
+    const child = this.heap[childIdx];
+    if (current >= child) {
+      this.heap[idx] = child;
+      this.heap[childIdx] = parent;
+      this._heapify(childIdx);
+    }
+  }
+}
